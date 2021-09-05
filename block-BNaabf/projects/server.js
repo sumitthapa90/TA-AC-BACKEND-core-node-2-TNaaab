52ccf91ab85616b27c48fc1eb470082c4dc00111
sumit <sumitthapa90@gmail.com>
2021-09-05 23:09:24 +0530

    BNaabf

--- block-BNaabf/projects/server.js
+++ block-BNaabf/projects/server.js
@@ -1,14 +1,7 @@
-var relativePath = "../clint/index.js";
-console.log(relativePath);
-
-var absolutePath = __dirname;
-console.log(__dirname + "../clint/index.js");
-
-
-
 var http = require("http");
 var fs = require("fs");
-var qs = require("querystring");
+var path = require("path");
+const { strict } = require("assert");
 
 var server = http.createServer(handleRequest);
 
@@ -18,22 +11,17 @@ function handleRequest(req, res) {
     store = store + chunk;
   });
   req.on("end", () => {
-    if (req.method === "GET" && req.url === "./form.html") {
-      fs.readFile("./form", (err, content) => {
-        if (err) console.log(err);
-        res.end(content);
-      });
-
-      if (req.method === "POST" && req.url === "./form.html") {
-        let parsedData = qs.parse(store);
-        res.setHeader("Content-Type", "text/html");
-        res.end(
-          `<h2> name: ${parsedData.name}</h2><h3>email:${parsedData.email}</h3> <h4>${parsedData.age}</h4>`
-        );
-      }
+    if (req.method === "GET" && req.url === "./form") {
+      res.setHeader("Content-Type", "text/html");
+      fs.createReadStream("./form.html").pipe(res);
+    }
+    if (req.method === "POST" && req.url === "./form") {
+      var parsedData = qs.parse(store);
+      res.setHeader("Content-Type", "text/html");
+      res.end(`<h2>${parsedData.name}</h2> <h3>${parsedData.email}</h3><h4>${parsedData.age}</h4>`)
     }
   });
 }
-server.listen(5000, () => {
-  console.log("Server listin at port 5000");
+server.listen(5678, () => {
+  console.log("Server listin at port 5678");
 });
